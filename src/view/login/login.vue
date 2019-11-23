7db232102bd0d0fe445db84333ed2e468a5a6679
demoklis <demoklis@163.com>
2019-11-23 22:27:50 +0800

    test

--- src/view/login/login.vue
+++ src/view/login/login.vue
@@ -5,7 +5,7 @@
 <template>
   <div class="login">
     <div class="login-con">
-      <Card icon="log-in" title="欢迎登录212121" :bordered="false">
+      <Card icon="log-in" title="欢迎登录21212" :bordered="false">
         <div class="form-con">
           <login-form @on-success-valid="handleSubmit"></login-form>
           <p class="login-tip">输入任意用户名和密码即可</p>
